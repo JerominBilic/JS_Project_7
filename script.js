9265b59a61edc2a10c8fb6fd954a1b7029d32822
JerominBilic <85135677+JerominBilic@users.noreply.github.com>
2021-06-08 09:36:50 -0600

    Using Javascript function

---
+++ script.js
@@ -0,0 +1,12 @@
+/*Add a simple function inside a script tag which sums the numbers between 1 and 100 inclusive.
+Print the sum using the log method.*/
+
+function sum(){
+    var total = 0;
+    for (var x = 0; x<=100; x++){
+        total += x;
+    }
+    console.log(total);
+}
+
+sum();
